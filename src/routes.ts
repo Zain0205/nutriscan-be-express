bc92b301bb7d0c192fc8e375f06f3a752218989d
Fahmi Zain <fahmizain000@gmail.com>
2025-03-02 21:17:02 +0700

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import authRoutes from "./auth/auth_routes"
 import scannerRoutes from "./scanner/scanner_routes"
+import foodRoutes from "./food/food_routes"
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.get("/api/check", (req: Request, res: Response) => {
 
 router.use(authRoutes);
 router.use(scannerRoutes);
+router.use(foodRoutes);
 
 export default router;
