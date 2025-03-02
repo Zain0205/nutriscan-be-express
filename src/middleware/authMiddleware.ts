c0decb0a548beb10cda569fd5c735a878e720583
Fahmi Zain <fahmizain000@gmail.com>
2025-03-02 21:16:58 +0700

    Create authMiddleware.ts

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { Request, Response, NextFunction } from 'express';
+
+dotenv.config();
+
+interface AuthenticatedRequest extends Request {
+  user?: any;
+}
+
+const authMiddleware = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  try {
+    const token = req.cookies?.token;
+    if (!token) {
+      return res.status(403).json({ message: 'Token required' });
+    }
+
+    const user = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: 'Invalid token' });
+  }
+};
+
+export default authMiddleware;
