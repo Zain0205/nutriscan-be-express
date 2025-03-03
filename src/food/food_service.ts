1fd3227232d897ad6c31b69dfdbad002825ddb83
Fahmi Zain <fahmizain000@gmail.com>
2025-03-03 14:32:16 +0700

    feat: create get daily foodlog

--- src/food/food_service.ts
+++ src/food/food_service.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../../prisma/PrismaClient";
+import { AuthenticatedRequest } from "../middleware/authMiddleware";
 import { IFood } from "./food_interface";
 
 export class FoodService {
@@ -19,30 +20,62 @@ export class FoodService {
     try {
       const newFoodData = await this.createFoodData(foodData);
 
-      const totalCal: number = newFoodData.calories
-      const totalProtein: number = newFoodData.protein
-      const totalFat: number = newFoodData.fat
-      const totalCarbs: number = newFoodData.carbs
+      const totalCal: number = newFoodData.calories;
+      const totalProtein: number = newFoodData.protein;
+      const totalFat: number = newFoodData.fat;
+      const totalCarbs: number = newFoodData.carbs;
 
       const newFoodLogData = await prisma.foodLog.create({
         data: {
-          userId: 1,
+          userId: userId,
           foodId: newFoodData.id,
           totalCal: totalCal,
           totalProtein: totalProtein,
           totalFat: totalFat,
           totalCarbs: totalCarbs,
-          quantity: 1
-        }
-      })
+          quantity: 1,
+        },
+      });
 
       return {
         food: newFoodData,
-        log: newFoodLogData
-      }
+        log: newFoodLogData,
+      };
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getDailyFoodLog(req: AuthenticatedRequest) {
+    try {
+      const userId = req.user.id;
+      const { date } = req.params;
+
+      const startDate = new Date(date)
+      startDate.setHours(0, 0, 0, 0);
+
+      const endDate = new Date(date)
+      endDate.setHours(23, 59, 59, 999);
+
+      const foodLogDataResult = await prisma.foodLog.findMany({
+        where:{
+          userId: userId,
+          createdAt: {
+            gte: startDate,
+            lte: endDate,
+          },
+        },
+        include: {
+          food: true,
+        },
+      })
+
+      return foodLogDataResult;
 
     } catch (err) {
       throw err;
     }
   }
+  async getWeeklyFoodLog() {}
+  async getMonthlyFoodLog() {}
 }
