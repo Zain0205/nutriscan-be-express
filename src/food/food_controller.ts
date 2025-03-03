28f382341af58b48d0b39aa2d7ec363e8dc5fded
Fahmi Zain <fahmizain000@gmail.com>
2025-03-03 14:32:35 +0700

    Update food_controller.ts

--- src/food/food_controller.ts
+++ src/food/food_controller.ts
@@ -1,25 +1,37 @@
 import { FoodService } from "./food_service";
 import { Request, Response } from "express";
+import { AuthenticatedRequest } from "../middleware/authMiddleware";
 
 export class FoodController {
   private foodService: FoodService;
 
-  constructor(){
+  constructor() {
     this.foodService = new FoodService();
   }
-  
-  async getFoodLogData(req: Request, res: Response){
+
+  async getFoodLogData(req: AuthenticatedRequest, res: Response) {
     try {
       const foodData = req.body;
-      const userId = req.body.userId;
+      const userId = req.user.id;
 
       const result = await this.foodService.getFoodLogData(foodData, userId);
       res.status(200).json(result);
-    }catch(err){
+    } catch (err) {
       console.error(err);
       res.status(400).json({
-        message: "Failed to get food log data"
-      })
+        message: "Failed to get food log data",
+      });
+    }
+  }
+
+  async getDailyFoodLog(req: AuthenticatedRequest, res: Response) {
+    try {
+
+    } catch (err) {
+      console.error(err);
+      res.status(400).json({
+        message: "Failed to get daily food log",
+      });
     }
   }
 }
